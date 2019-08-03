36c3ea75f1395a2911d344b1dca75f07948a5d0f
Paolo Rotolo <rotolopao@gmail.com>
2019-08-03 19:55:52 +0200

    Add recyclerview

--- app/src/main/java/com/github/rotolonico/taskapp/MainActivity.kt
+++ app/src/main/java/com/github/rotolonico/taskapp/MainActivity.kt
@@ -2,11 +2,30 @@ package com.github.rotolonico.taskapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.rotolonico.taskapp.models.Project
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var adapter: ProjectsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
+
+        initRecycler()
+    }
+
+    fun updateData(projects: List<Project>){
+        adapter.apply {
+            data = projects
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun initRecycler() {
+        projectsRecycler.layoutManager = LinearLayoutManager(this)
+        projectsRecycler.adapter = ProjectsAdapter(ArrayList())
     }
 }
--- app/src/main/java/com/github/rotolonico/taskapp/ProjectsAdapter.kt
+++ app/src/main/java/com/github/rotolonico/taskapp/ProjectsAdapter.kt
@@ -1,2 +1,30 @@
 package com.github.rotolonico.taskapp
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.rotolonico.taskapp.models.Project
+import kotlinx.android.synthetic.main.item_project.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ProjectsAdapter(var data: List<Project>): RecyclerView.Adapter<ProjectsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_project, parent, false) as View
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.view.projectTitle.text = data[position].name
+        holder.view.projectCreated.text = timestampToReadable(data[position].creation)
+    }
+
+    fun timestampToReadable(timestamp: Long): String? = SimpleDateFormat("dd MM yyyy").format(Date(timestamp))
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+}
